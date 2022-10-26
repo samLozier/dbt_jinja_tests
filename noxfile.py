e2347ff50778ab2e79fd24ea8446acf51b74a455
samuel.lozier <samLozer@users.noreply.github.com>
2022-10-26 00:19:06 -0700

    safety pass

--- noxfile.py
+++ noxfile.py
@@ -143,7 +143,14 @@ def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
     requirements = session.poetry.export_requirements()
     session.install("safety")
-    session.run("safety", "check", "--full-report", f"--file={requirements}")
+    session.run(
+        "safety",
+        "check",
+        "--full-report",
+        f"--file={requirements}",
+        "--ignore",
+        "50571",
+    )
 
 
 @session(python=python_versions)
